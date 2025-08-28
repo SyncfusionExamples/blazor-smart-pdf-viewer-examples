cfd6d274f8e796d8bf287023ef62a2eb0c4d4062
Hemaprasanth-Hari <hemaprasanth.hari@syncfusion.com>
2025-08-28 21:17:51 +0530

    Add the samples for the smart redaction

---
+++ Smart Redaction/SmartRedaction/Program.cs
@@ -0,0 +1,39 @@
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using SmartRedaction.Components;
+using Syncfusion.Blazor.AI;
+using System.ClientModel;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+string azureOpenAiKey = "enter-your-key";
+string azureOpenAiEndpoint = "enter-your-end-point";
+string azureOpenAiModel = "enter-your-model-name";
+AzureOpenAIClient azureOpenAIClient = new AzureOpenAIClient(new Uri(azureOpenAiEndpoint), new ApiKeyCredential(azureOpenAiKey));
+IChatClient azureOpenAiChatClient = azureOpenAIClient.GetChatClient(azureOpenAiModel).AsIChatClient();
+builder.Services.AddChatClient(azureOpenAiChatClient);
+builder.Services.AddSingleton<IChatInferenceService, SyncfusionAIService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
