b8d9468fb0599494010a6dc4436f20724bccc8f9
DivyaUdhayakumar <divya.udhayakumar@syncfsuion.com>
2025-08-28 20:49:46 +0530

    977776: Updated Readme file for Smart Fill, RTL and Summarizer Samples

--- DocumentSummarizer/InitialPromptSettings/Program.cs
+++ DocumentSummarizer/InitialPromptSettings/Program.cs
@@ -24,7 +24,7 @@ public class Program
 
         string azureOpenAiKey = "enter-your-key";
         string azureOpenAiEndpoint = "enter - your - endpoint";
-        string azureOpenAiModel = "gpt-4o-mini";
+        string azureOpenAiModel = "enter-your-model-name";
         AzureOpenAIClient azureOpenAIClient = new AzureOpenAIClient(new Uri(azureOpenAiEndpoint), new ApiKeyCredential(azureOpenAiKey));
         IChatClient azureOpenAiChatClient = azureOpenAIClient.GetChatClient(azureOpenAiModel).AsIChatClient();
         builder.Services.AddChatClient(azureOpenAiChatClient);
