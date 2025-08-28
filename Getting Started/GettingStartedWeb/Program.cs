cecf73fdf353e7d22db26642cb79b0f2cb2b7342
lingeshwaran-sf4984 <lingeshwaran.sankar@syncfusion.com>
2025-08-28 17:28:52 +0530

    977889: revert changes

--- Getting Started/GettingStartedWeb/Program.cs
+++ Getting Started/GettingStartedWeb/Program.cs
@@ -22,9 +22,9 @@ public class Program
         builder.Services.AddMemoryCache();
         builder.Services.AddSyncfusionBlazor();
 
-        string apiKey = "C5PujTMQv71JwaQsthRcGNxopR78M5SbftbOQxN2oALxDN0bBSBrJQQJ99BHACYeBjFXJ3w3AAABACOGgpDM";
-        string deploymentName = "gpt-4o-mini";
-        string endpoint = "https://openai-165589.openai.azure.com/";
+        string apiKey = "api-key";
+        string deploymentName = "deployment-name";
+        string endpoint = "endpoint URL";
 
         string azureOpenAiKey = apiKey;
         string azureOpenAiEndpoint = endpoint;
