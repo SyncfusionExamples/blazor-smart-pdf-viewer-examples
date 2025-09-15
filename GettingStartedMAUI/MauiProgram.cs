9bf28103e6cd138e9d3a1d9ffbf92650397f54cb
Yaminisrisf4389 <yaminisri.m@syncfusion.com>
2025-09-15 17:06:17 +0530

    980582: Add_documentation_for_Blazor_MAUI_APP

---
+++ GettingStartedMAUI/MauiProgram.cs
@@ -0,0 +1,45 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.AI;
+using System.ClientModel;
+
+namespace GettingstartedMAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMemoryCache();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            string azureOpenAiKey = "Your API Key";
+            string azureOpenAiEndpoint = "Your End Point";
+            string OpenAiModel = "Your Model name";
+
+            AzureOpenAIClient azureOpenAIClient = new AzureOpenAIClient(new Uri(azureOpenAiEndpoint), new ApiKeyCredential(azureOpenAiKey));
+            IChatClient azureOpenAiChatClient = azureOpenAIClient.GetChatClient(OpenAiModel).AsIChatClient();
+            builder.Services.AddChatClient(azureOpenAiChatClient);
+
+            builder.Services.AddSingleton<IChatInferenceService, SyncfusionAIService>();
+            builder.Services.AddSyncfusionBlazor();
+
+            return builder.Build();
+        }
+
+    }
+}
