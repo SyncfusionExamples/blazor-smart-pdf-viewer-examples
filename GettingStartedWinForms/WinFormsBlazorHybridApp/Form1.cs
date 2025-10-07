74a4a370ada7d2b38f576550cc6e39041191ea9d
Tamilselvan-Durairaj <153176971+Tamilselvan-Durairaj@users.noreply.github.com>
2025-10-07 15:48:58 +0530

    984779: Update the smart pdfviewer samples

--- GettingStartedWinForms/WinFormsBlazorHybridApp/Form1.cs
+++ GettingStartedWinForms/WinFormsBlazorHybridApp/Form1.cs
@@ -13,7 +13,7 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
-        var services = new ServiceCollection();
+        ServiceCollection services = new ServiceCollection();
         services.AddWindowsFormsBlazorWebView();
         services.AddMemoryCache();
         services.AddSyncfusionBlazor();
@@ -24,7 +24,7 @@ public partial class Form1 : Form
         IChatClient azureOpenAiChatClient = azureOpenAIClient.GetChatClient(azureOpenAiModel).AsIChatClient();
         services.AddChatClient(azureOpenAiChatClient);
         services.AddSingleton<IChatInferenceService, SyncfusionAIService>();
-        var blazorWebView = new BlazorWebView()
+        BlazorWebView blazorWebView = new BlazorWebView()
         {
             HostPage = "wwwroot\\index.html",
             Services = services.BuildServiceProvider(),
